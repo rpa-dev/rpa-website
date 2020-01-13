d31aa8ad5aa32908116eb413cf0d5f9a4d28f8e3
willhlaw <will.lawrence@gmail.com>
2020-01-13 00:12:09 -0500

    Add Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -78,6 +78,12 @@ module.exports = {
       }
     },
     {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-156077796-1",
+      },
+    },
+    {
       resolve: 'gatsby-plugin-purgecss', // purges all unused/unreferenced css rules
       options: {
         develop: true, // Activates purging in npm run develop
